d7e93212c3d1877c182fa7608177daacd933af26
🐼💭 💙 <haochi_no_douhua@noMacBook-Pro.local>
2020-08-22 22:10:43 +0900

    tab完成

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ $(function(){
   $('.tab li').on('click',function(){
     $('.tab li').removeClass('select');
     $(this).addClass('select');
+
+    $('.content').addClass('hide');
+
+    let index = $('.tab li').index($(this));
+    console.log(index)
+
+    $('.content').eq(index).removeClass('hide');
   });
 });
